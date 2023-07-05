bccd58e93af4037f5632c391c8de630941679517
Manuel David Castillo <manuelda27999@gmail.com>
2023-07-05 17:55:26 +0200

    reactive footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -52,43 +52,47 @@
 		</a>
 	</div>
 	<div class="bg-base-300 md:p-16 px-6 pt-9 pb-6 md:pb-8">
-		<!-- <div class="flex justify-start mr-72">
-			<div class="flex flex-col justify-center">
-				<img src="../images/logo.png" alt="" class="aspect-1/1 w-16" />
+		<div class="flex justify-start max-md:flex-col md:pb-14 pb-8">
+			<div class="flex justify-center items-center">
+				<img src="../images/logo.png" alt="" class="aspect-auto md:w-16 w-0 md:h-16 h-0" />
 			</div>
-			<div class="flex flex-col gap-4 items-start mx-24">
+			<div class="flex flex-col md:space-y-6 space-y-4 items-start md:mx-24 mx-0 md:mb-0 mb-8">
 				<p class="text-sm text-primary-content">Company</p>
-				<a class="text-base-200 text-xs" href="/">Home</a>
-				<a class="text-base-200 text-xs" href="/">Capabilies</a>
-				<a class="text-base-200 text-xs" href="/">Web 3</a>
-				<a class="text-base-200 text-xs" href="/">About us</a>
+				<a class="text-base-200 text-sm" href="/">Home</a>
+				<a class="text-base-200 text-sm" href="/">Capabilies</a>
+				<a class="text-base-200 text-sm" href="/">Web 3</a>
+				<a class="text-base-200 text-sm" href="/">About us</a>
 			</div>
-			<div class="flex flex-col gap-4 items-start">
+			<div class="flex flex-col md:space-y-6 space-y-4 items-start md:mb-0 mb-4">
 				<p class="text-sm text-primary-content">Services</p>
-				<a class="text-base-200 text-xs" href="/">UX/UI desing</a>
-				<a class="text-base-200 text-xs" href="/">Web development</a>
-				<a class="text-base-200 text-xs" href="/">Movile development</a>
-				<a class="text-base-200 text-xs" href="/">Technical consulting</a>
+				<a class="text-base-200 text-sm" href="/">UX/UI desing</a>
+				<a class="text-base-200 text-sm" href="/">Web development</a>
+				<a class="text-base-200 text-sm" href="/">Movile development</a>
+				<a class="text-base-200 text-sm" href="/">Technical consulting</a>
 			</div>
-			<div class="flex flex-col justify-end gap-4 items-start">
-				<a class="text-base-200 text-xs" href="/">Custom corporate web</a>
-				<a class="text-base-200 text-xs" href="/">Tracker integrations</a>
-				<a class="text-base-200 text-xs" href="/">Web3 adoption consulting</a>
-				<a class="text-base-200 text-xs" href="/">Web3 tools integrations</a>
+			<div
+				class="flex flex-col justify-end md:space-y-6 space-y-4 items-start md:mx-24 mx-0 md:mb-0 mb-4"
+			>
+				<a class="text-base-200 text-sm" href="/">Custom corporate web</a>
+				<a class="text-base-200 text-sm" href="/">Tracker integrations</a>
+				<a class="text-base-200 text-sm" href="/">Web3 adoption consulting</a>
+				<a class="text-base-200 text-sm" href="/">Web3 tools integrations</a>
 			</div>
-			<div class="flex flex-col justify-end gap-4 items-start">
-				<a class="text-base-200 text-xs" href="/">Web3 protocol development</a>
-				<a class="text-base-200 text-xs" href="/">Smart contracts development</a>
-				<a class="text-base-200 text-xs" href="/">NFTs solutions</a>
-				<a class="text-base-200 text-xs" href="/">Web3 adits</a>
+			<div
+				class="flex flex-col justify-end md:space-y-6 space-y-4 md:mr-24 mr-0 items-start md:mb-0 mb-8"
+			>
+				<a class="text-base-200 text-sm" href="/">Web3 protocol development</a>
+				<a class="text-base-200 text-sm" href="/">Smart contracts development</a>
+				<a class="text-base-200 text-sm" href="/">NFTs solutions</a>
+				<a class="text-base-200 text-sm" href="/">Web3 adits</a>
 			</div>
-			<div class="flex flex-col gap-4 items-start">
+			<div class="flex flex-col md:space-y-6 space-y-4 items-start">
 				<p class="text-sm text-primary-content">Products</p>
-				<a class="text-base-200 text-xs" href="/">Kudu pay</a>
-				<a class="text-base-200 text-xs" href="/">Wallet Kudu</a>
-				<a class="text-base-200 text-xs" href="/">Second Hand</a>
+				<a class="text-base-200 text-sm" href="/">Kudu pay</a>
+				<a class="text-base-200 text-sm" href="/">Wallet Kudu</a>
+				<a class="text-base-200 text-sm" href="/">Second Hand</a>
 			</div>
-		</div> -->
+		</div>
 		<div class="divider max-xs:w-screen max-xs:!-ml-6" />
 		<ul class="flex justify-center md:py-8 py-4 space-x-4">
 			<!-- <li>
@@ -119,21 +123,23 @@
 			</li> -->
 		</ul>
 		<div class="divider max-xs:w-screen max-xs:!-ml-6" />
-		<!-- <div class="flex justify-between items-center pb-4">
-			<p class="mx-12 text-xs">Copyright © 2023, Kudu Consultant LLC</p>
-			<div class="mx-12 flex align-middle items-center">
-				<a class=" px-1 text-xs text-primary-content" href="/">Cookie Management</a>
-				<div class="h-3 border-l border-white" />
-				<a class=" px-1 text-xs text-primary-content" href="/">Privacy Policy</a>
-				<div class="h-3 border-l border-white" />
-				<a class=" px-1 text-xs text-primary-content" href="/">Terms of Use</a>
+		<div
+			class="flex md:justify-between md:items-center md:pt-8 max-md:flex-col-reverse max-md:mt-8 max-md:justify-start"
+		>
+			<p class="max-md:mt-8 text-sm">Copyright © 2023, Kudu Consultant LLC</p>
+			<div class="flex align-middle md:items-center max-md:flex-col-reverse">
+				<a class="px-1 text-sm text-primary-content" href="/">Cookie Management</a>
+				<div class="md:h-3 h-0 border-l border-white" />
+				<a class="px-1 text-sm text-primary-content max-md:my-4" href="/">Privacy Policy</a>
+				<div class="md:h-3 h-0 border-l border-white" />
+				<a class="px-1 text-sm text-primary-content" href="/">Terms of Use</a>
 				<select
-					class="bg-base-300 rounded-xl border-white border p-1 text-xs ml-2 flex justify-center"
+					class="bg-base-300 rounded-xl border-white border p-1 text-sm flex justify-center md:ml-8 max-md:mb-8 max-md: w-20"
 				>
-					<option class="text-xs" value="englis">English</option>
-					<option class="text-xs" value="spanish">Spanish</option>
+					<option class="text-sm" value="englis">English</option>
+					<option class="text-sm" value="spanish">Spanish</option>
 				</select>
 			</div>
-		</div> -->
+		</div>
 	</div>
 </footer>
